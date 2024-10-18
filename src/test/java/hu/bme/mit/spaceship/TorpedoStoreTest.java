f215fdb60ce48ffed21caddfc63d23545374ffc0
Szmoleniczki Ákos <145291965+szmoli@users.noreply.github.com>
2024-10-18 13:38:09 +0200

    unit tests

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,6 +1,7 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 class TorpedoStoreTest {
@@ -15,5 +16,24 @@ class TorpedoStoreTest {
 
         // Assert
         assertEquals(true, result);
+    }
+
+    @Test
+    void getTorpedoCount_Success() {
+        TorpedoStore store = new TorpedoStore(1);
+        int result = store.getTorpedoCount();
+        assertEquals(1, result);
+    }
+
+    @Test
+    void fire_Exception() {
+        TorpedoStore store = new TorpedoStore(1);
+        Exception result = assertThrows(IllegalArgumentException.class, () -> store.fire(2));
+    }
+
+    @Test
+    void TorpedoStore_failureEnv() {
+        System.getenv().put("IVT_FAILURE", "asd");
+        TorpedoStore store = new TorpedoStore(1);
     }
 }
